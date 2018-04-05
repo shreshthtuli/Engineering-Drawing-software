1c1c2d7125dbbcb3f713b8501cba3ef5ac149a84
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-04-05 07:58:16 +0530

    final commit

---
+++ doc/doc_src/shaders.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+using namespace std;
+
+///Program Error raising function
+void programerrors(const GLint program)
+{
+
+}
+
+//Shader Errors raising function
+void shadererrors(const GLint shader)
+{
+
+}
+
+//OpenGL Shaders initialization
+GLuint initshaders(GLenum type, const char *filename)
+{
+
+}
+
+//OpenGL program intialization
+GLuint initprogram(GLuint vertexshader, GLuint fragmentshader)
+{
+
+}
